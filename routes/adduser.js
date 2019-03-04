ec04ec6581f1effbbc9dc4a70d1cf024db3c135a
Valentina Pogudina <valentina.pogudina@stonybrook.edu>
2019-03-04 00:06:37 -0500

    getting started with mondodb

--- routes/adduser.js
+++ routes/adduser.js
@@ -12,6 +12,32 @@ router.post('/', function(req, res, next) {
     var pwd = req.body.password;
     var email = req.body.email;
     console.log(name, pwd, email);
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://localhost:27017/";
 
+    // Check if the user already exists
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("ttt");
+        var query = { name: name };
+        dbo.collection("users").find(query).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+        });
+    });
+
+    /*
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("ttt");
+        var myobj = { name: "Company Inc", address: "Highway 37" };
+        dbo.collection("users").insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });
+    });
+    */
 });
 
