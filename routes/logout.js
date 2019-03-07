344baae45925d06ef3ec23f6ca6de72c690814c2
Valentina Pogudina <valentina.pogudina@stonybrook.edu>
2019-03-07 15:42:35 -0500

    logout

--- routes/logout.js
+++ routes/logout.js
@@ -4,33 +4,28 @@ const Users = require('../models/Users');
 var mongoStore = require('../mongoSess');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     res.send('respond with a resource');
 });
 
 //logout
 
-router.post('/', function(req, res, next) {
-    console.log("               ");
-    console.log("=======================");
-    console.log(req.body);
-    console.log("========================");
-    console.log("               ");
-    console.log("session.userid "+req.session.userId);
-    if(!req.session.userId) {
+router.post('/', function (req, res, next) {
+    console.log("session.userid " + req.session.userId);
+    if (!req.session.userId) {
         console.log("               ");
         console.log("=======================");
         res.json({status: 'ERROR'});
         console.log("========================");
         console.log("               ");
         res.json({status: 'ERROR'});
-        return console.log('ERROR');
+    } else {
+        res.clearCookie('tictactoe');
+        console.log("               ");
+        console.log("=======================");
+        res.json({status: 'OK'});
+        console.log("========================");
+        console.log("               ");
     }
-    res.clearCookie('tictactoe');
-    console.log("               ");
-    console.log("=======================");
-    res.json({status: 'OK'});
-    console.log("========================");
-    console.log("               ");
 });
 module.exports = router;
